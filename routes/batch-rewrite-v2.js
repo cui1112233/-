014450592f8ee6a786632473f8348cbcef374ee5
cui1112233 <732223510@qq.com>
2026-08-31 18:56:25 +0800

    feat(novel-fetch): add V78 V2 router adapter

---
+++ routes/batch-rewrite-v2.js
@@ -0,0 +1,12 @@
+const { registerNovelFetchV2Routes } = require('../lib/novel-fetch-workshop/v2-api-contract');
+
+function createBatchRewriteV2Router({ queue, scheduler, auth, routerFactory } = {}) {
+  const createRouter = routerFactory || (() => require('express').Router());
+  const router = createRouter();
+  const authMiddleware = auth || require('../middleware/auth').apiAuth;
+  router.use(authMiddleware);
+  registerNovelFetchV2Routes(router, { queue, scheduler });
+  return router;
+}
+
+module.exports = { createBatchRewriteV2Router };
