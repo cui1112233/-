5359786872c8a9105a8d7aa510a80bdbbcc97988
cui1112233 <732223510@qq.com>
2026-08-31 19:06:35 +0800

    feat(novel-fetch): inject task operations into V2 router

--- routes/batch-rewrite-v2.js
+++ routes/batch-rewrite-v2.js
@@ -1,11 +1,11 @@
 const { registerNovelFetchV2Routes } = require('../lib/novel-fetch-workshop/v2-api-contract');
 
-function createBatchRewriteV2Router({ queue, scheduler, auth, routerFactory } = {}) {
+function createBatchRewriteV2Router({ queue, scheduler, taskOps, auth, routerFactory } = {}) {
   const createRouter = routerFactory || (() => require('express').Router());
   const router = createRouter();
   const authMiddleware = auth || require('../middleware/auth').apiAuth;
   router.use(authMiddleware);
-  registerNovelFetchV2Routes(router, { queue, scheduler });
+  registerNovelFetchV2Routes(router, { queue, scheduler, taskOps });
   return router;
 }
 
