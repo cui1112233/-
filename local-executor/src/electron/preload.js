b4de75a3879b34e587096cdd1e5e3ea0b87c8f70
cui1112233 <732223510@qq.com>
2026-09-01 17:02:04 +0800

    feat(executor): expose narrow desktop UI bridge

---
+++ local-executor/src/electron/preload.js
@@ -0,0 +1,14 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('yiZhanExecutor', {
+  getState: () => ipcRenderer.invoke('executor:get-state'),
+  pair: input => ipcRenderer.invoke('executor:pair', input),
+  addAccount: () => ipcRenderer.invoke('executor:add-account'),
+  openAccount: id => ipcRenderer.invoke('executor:open-account', id),
+  markAccountAvailable: id => ipcRenderer.invoke('executor:mark-account-available', id),
+  onState: callback => {
+    const listener = (_event, state) => callback(state);
+    ipcRenderer.on('executor:state', listener);
+    return () => ipcRenderer.removeListener('executor:state', listener);
+  }
+});
