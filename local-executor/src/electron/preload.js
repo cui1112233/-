3c10ae40c6c10e02d3bae21aa9b103b67eb50b84
cui1112233 <732223510@qq.com>
2026-09-01 18:33:48 +0800

    feat(executor): expose automation toggle to desktop UI

--- local-executor/src/electron/preload.js
+++ local-executor/src/electron/preload.js
@@ -6,6 +6,7 @@ contextBridge.exposeInMainWorld('yiZhanExecutor', {
   addAccount: () => ipcRenderer.invoke('executor:add-account'),
   openAccount: id => ipcRenderer.invoke('executor:open-account', id),
   markAccountAvailable: id => ipcRenderer.invoke('executor:mark-account-available', id),
+  setAutomationEnabled: enabled => ipcRenderer.invoke('executor:set-automation-enabled', Boolean(enabled)),
   onState: callback => {
     const listener = (_event, state) => callback(state);
     ipcRenderer.on('executor:state', listener);
