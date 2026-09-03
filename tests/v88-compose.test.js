fece36a697ee635077a476fa6e1aee91182640a5
cui1112233 <732223510@qq.com>
2026-09-03 10:24:22 +0800

    test(v88): validate explicit worker xvfb launcher

--- tests/v88-compose.test.js
+++ tests/v88-compose.test.js
@@ -5,6 +5,7 @@ const path = require('node:path');
 
 const composePath = path.join(__dirname, '..', 'docker-compose.v88-review.yml');
 const envPath = path.join(__dirname, '..', '.env.v88-review.example');
+const workerDockerfilePath = path.join(__dirname, '..', 'services', '121-browser-worker', 'Dockerfile');
 
 test('V88 review compose wires platform, Go, MySQL, and private 121 worker safely', () => {
   const yaml = fs.readFileSync(composePath, 'utf8');
@@ -19,14 +20,15 @@ test('V88 review compose wires platform, Go, MySQL, and private 121 worker safel
   assert.match(yaml, /QIANTIE_MYSQL_DSN:.*@tcp\(mysql:3306\)/);
   assert.match(yaml, /QIANTIE_BATCH_FACTORY_V11_SLICE:\s*\$\{QIANTIE_V88_SLICE:-4\}/);
   const backendSection = yaml.split(/\n  backend:\s*/)[1]?.split(/\n  platform:\s*/)[0] || '';
-  assert.match(backendSection, /healthcheck:[\s\S]*test:[\s\S]*\/qiantie["']?,?\s*["']?healthcheck/);
+  assert.match(backendSection, /healthcheck:[\s\S]*test:[\s\S]*\/qiantie[\s\S]*healthcheck/);
   assert.match(yaml, /novel-fetch-121-worker:[\s\S]*?expose:\s*\n\s*- ["']?8787/);
   const workerSection = yaml.split(/\n  novel-fetch-121-worker:\s*/)[1] || '';
   assert.doesNotMatch(workerSection.split(/\n  [a-z0-9-]+:\s*\n/)[0], /\n\s+ports:/);
   assert.match(yaml, /QIANTIE_121_HEADED_ENABLED:\s*["']1["']/);
   assert.match(yaml, /x-qiantie-internal-secret/);
-  assert.match(yaml, /COPY start-worker\\.sh \.\\/start-worker\\.sh/);
-  assert.match(yaml, /CMD \\[["']\\.\\/start-worker\\.sh["']\\]/);
+  const workerDockerfile = fs.readFileSync(workerDockerfilePath, 'utf8');
+  assert.match(workerDockerfile, /COPY start-worker\.sh \.\/start-worker\.sh/);
+  assert.match(workerDockerfile, /CMD \[["']\.\/start-worker\.sh["']\]/);
   for (const variable of ['QIANTIE_V88_DATA_DIR', 'QIANTIE_V88_MYSQL_DATA_DIR', 'QIANTIE_V88_WORKER_DATA_DIR', 'QIANTIE_V88_BRIDGE_SECRET']) {
     assert.match(yaml, new RegExp('\\$\\{' + variable + ':\\?required\\}'));
   }
