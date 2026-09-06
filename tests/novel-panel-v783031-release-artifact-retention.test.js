654c418eba4332abc3211f6d4e7fe2988596f13b
cui1112233 <732223510@qq.com>
2026-09-06 19:45:30 +0800

    test: align release regression with multi-image publish step

--- tests/novel-panel-v783031-release-artifact-retention.test.js
+++ tests/novel-panel-v783031-release-artifact-retention.test.js
@@ -13,7 +13,7 @@ assert.match(workflow, /permissions:\s*[\s\S]*?contents:\s*read[\s\S]*?actions:\
 
 const cleanupMarker = '- name: Prune old V88 AMD64 release artifacts';
 const loginMarker = '- name: Login to GHCR';
-const pushMarker = '- name: Push V88 AMD64 image to GHCR';
+const pushMarker = '- name: Push V88 AMD64 images to GHCR';
 const uploadMarker = '- name: Upload V88 AMD64 release artifact';
 assert.ok(workflow.includes(cleanupMarker), 'release workflow must prune old V88 AMD64 artifacts before upload');
 assert.ok(workflow.indexOf(cleanupMarker) < workflow.indexOf(uploadMarker), 'artifact pruning must run before upload');
